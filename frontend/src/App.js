1a29621ed77fa01c33d436267eef06f86e6347ba
Darshana R S <darshanarmp@gmail.com>
2024-08-11 19:31:37 +0530

    committed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
+import { useEffect } from 'react';
 import AppRoutes from './AppRoutes';
 import Header from './components/Header/Header';
 import Loading from './components/Loading/Loading';
 import { useLoading } from './hooks/useLoading';
 import { setLoadingInterceptor } from './interceptors/loadingInterceptor';
-import { useEffect } from 'react';
 
 function App() {
   const { showLoading, hideLoading } = useLoading();
@@ -22,3 +22,4 @@ function App() {
 }
 
 export default App;
+//App.js for BuyBooks
